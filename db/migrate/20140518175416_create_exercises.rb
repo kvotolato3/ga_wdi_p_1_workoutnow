68f585d7c87d8adc47a304f2615d74fd46999ab7
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-18 15:36:54 -0400

    Add category to Exercises and create seed data

--- db/migrate/20140518175416_create_exercises.rb
+++ db/migrate/20140518175416_create_exercises.rb
@@ -4,10 +4,11 @@ class CreateExercises < ActiveRecord::Migration
       t.string :name
       t.integer :seconds_duration
       t.integer :repetitions
-      t.string :description
+      t.text :description
       t.string :equipment
       t.string :difficulty
       t.string :photo_url
+      t.string :category
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,217 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Exercise.delete_all
+Exercise.create(
+  name: 'Inchworm',
+  description: "Stand up tall with the legs straight, and do like Lil’ Jon and let
+  those fingertips hit the floor. Keeping the legs straight (but not locked!),
+  slowly lower the torso toward the floor, and then walk the hands forward. Once
+  in a push-up position, start taking tiny steps so the feet meet the hands.",
+  seconds_duration: 45,
+  repetitions: 5,
+  difficulty: 'Easy',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Inchworm_edited-300x300_0.jpg?itok=3W4N0Y-y",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Tuck Jump',
+  description: "Standing with the knees slightly bent, jump up as high as possible
+  (pretend Jeremy Lin is watching!) and bring the knees in toward the chest while extending
+  the arms straight out. Land with the knees slightly bent and quickly jump (on it) again!",
+  seconds_duration: 45,
+  repetitions: 5,
+  difficulty: 'Easy',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Inchworm_edited-300x300_0.jpg?itok=3W4N0Y-y",
+  category: "Bodyweight"
+  )
+
+
+Exercise.create(
+  name: 'Bear Crawl',
+  description: "Embrace that inner grizzly. Starting on the hands and knees, rise up onto
+  the toes, tighten the core, and slowly reach forward with the right arm and right knee, followed
+  by the left side. Continue the crawl for 8-10 reps (or until you scare your roommates off).",
+  seconds_duration: 60,
+  repetitions: 8,
+  difficulty: 'Easy',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Inchworm_edited-300x300_0.jpg?itok=3W4N0Y-y",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Plyometric Push-Up',
+  description: "Ready to catch some air? Start on a well-padded surface and complete a traditional
+  push-up. Then, in an explosive motion, push up hard enough to come off the floor (and hang ten for a
+    second!). Once back on solid ground, immediately head into the next repetition.",
+  seconds_duration: 120,
+  repetitions: 5,
+  difficulty: 'Hard',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Inchworm_edited-300x300_0.jpg?itok=3W4N0Y-y",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Stair Climb with Bicep Curl',
+  description: "Turn those stairs into a cardio machine — no magic wand necessary. Grab some dumbbells (or household
+    objects!) and briskly walk up and down the stairway while simultaneously doing bicep curls to work the whole body.",
+  seconds_duration: 300,
+  repetitions: 1,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Inchworm_edited-300x300_0.jpg?itok=3W4N0Y-y",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Mountain Climber',
+  description: "Starting on your hands and knees, bring the left foot forward directly under
+  the chest while straightening the right leg. Keeping the hands on the ground and core tight,
+  jump and switch legs. The left leg should now be extended behind the body with the right knee forward.
+  Next up? Everest.",
+  seconds_duration: 120,
+  repetitions: 8,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+Exercise.create(
+  name: 'Prone Walkout',
+  description: "Beginning on all fours with the core engaged, slowly walk the hands forward, staying on the toes but not moving them forward. Next, gradually walk the hands backwards to the starting position, maintain stability and balance. (This dance comes next.)",
+  seconds_duration: 60,
+  repetitions: 8,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Burpees',
+  description: "One of the most effective full-body exercises around, this one starts out in a low squat position with hands on the floor. Next, kick the feet back to a push-up position, complete one push-up, then immediately return the feet to the squat position. Leap up as high as possible before squatting and moving back into the push-up portion of the show.",
+  seconds_duration: 120,
+  repetitions: 10,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Plank',
+  description: "Nope, we’re (thankfully) not walking the plank. Lie face down with forearms on the floor and hands clasped. Extend the legs behind the body and rise up on the toes. Keeping the back straight, tighten the core and hold the position for 30-60 seconds (or as long as you can hang).",
+  seconds_duration: 60,
+  repetitions: 1,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Plank-to-Push-Up',
+  description: "Starting in a plank position, place down one hand at a time to lift up into a push-up position, with the back straight and the core engaged. Then move one arm at a time back into the plank position (forearms on the ground). Repeat, alternating the arm that makes the first move.",
+  seconds_duration: 120,
+  repetitions: 8,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Wall Sit',
+  description: "Who needs a chair when there’s a wall? Slowly slide your back down a wall until the thighs are parallel to the ground. Make sure the knees are directly above the ankles and keep the back straight. Go for 60 seconds per set (or however long it takes to turn those legs to jelly). Need more fire? Add some bicep curls.",
+  seconds_duration: 120,
+  repetitions: 2,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Wall-Sit_edited-300x300_0.jpg?itok=jPAmdn2x",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Lunge',
+  description: "Stand with the hands on the hips and feet hip-width apart. Step the right leg forward and slowly lower your body until the right knee is close to or touching the floor and bent at least 90 degrees. Return to the starting position and repeat with the left leg. Try stepping back into the lunge for a different variation.",
+  seconds_duration: 180,
+  repetitions: 20,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Clock Lunge',
+  description: "Time for a challenge. Complete a traditional forward lunge, then take a big step to the right and lunge again. Finish off the semicircle with a backwards lunge, then return to standing. And all that’s one rep! Aim for 10 reps and then switch legs.",
+  seconds_duration: 300,
+  repetitions: 20,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Lunge-to-Row',
+  description: "Start by doing a normal lunge. Instead of bringing that forward leg back to the starting position, raise it up off the floor while lifting the arms overhead. The leg should remain bent at about 90 degrees. Add weights to really bring the heat.",
+  seconds_duration: 300,
+  repetitions: 20,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Lung Jump',
+  description: "Ready to impress some friends? Stand with the feet together and lunge forward with the right foot. Jump straight up, propelling the arms forward while keeping the elbows bent. While in the air, switch legs and land in a lunge with the opposite leg forward. Repeat and continue switching legs. Try to do 10!",
+  seconds_duration: 300,
+  repetitions: 10,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Curtsy Lunge',
+  description: "Let’s show a little respect. When lunging, step the left leg back behind the right, bending the knees and lowering the hips until the right thigh is almost parallel to the floor. Remember to keep the torso upright and the hips square.",
+  seconds_duration: 300,
+  repetitions: 10,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
+
+Exercise.create(
+  name: 'Squat',
+  description: "Stand with the feet parallel or turned out 15 degrees — whatever is most comfortable. Slowly start to crouch by bending the hips and knees until the thighs are at least parallel to the floor. Make sure the heels do not rise off the floor. Press through the heels to return to a standing position.",
+  seconds_duration: 300,
+  repetitions: 10,
+  difficulty: 'Medium',
+  source_url: "http://greatist.com/fitness/50-bodyweight-exercises-you-can-do-anywhere",
+  equipment: 'None',
+  photo_url: "https://greatist.com/sites/default/files/styles/article-content-732/public/Mountain-Climber_edited-300x300_0.jpg?itok=ySL7Djjy",
+  category: "Bodyweight"
+  )
