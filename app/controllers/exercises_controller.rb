c620f19e642b6bfc834bb88fd3ec2f106ea81a6e
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-19 12:06:02 -0400

    Add exercises list to exercise index

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,8 @@
 class ExercisesController < ApplicationController
   def index
+    if params[:desired_category].present?
+      @exercises = Exercise.where(category: params[:desired_category])
+    end
   end
 end
+
