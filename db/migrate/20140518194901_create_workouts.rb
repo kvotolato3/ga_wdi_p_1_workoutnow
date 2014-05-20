62efff2e9be83712e22b5d6049cf2566fd4d5acf
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-20 14:23:32 -0400

    Set Workout default duration to 0

--- db/migrate/20140518194901_create_workouts.rb
+++ db/migrate/20140518194901_create_workouts.rb
@@ -2,7 +2,7 @@ class CreateWorkouts < ActiveRecord::Migration
   def change
     create_table :workouts do |t|
       t.string :name, default: "Untitled Workout"
-      t.integer :duration
+      t.integer :duration, default: 0
       t.references :user
 
       t.timestamps
