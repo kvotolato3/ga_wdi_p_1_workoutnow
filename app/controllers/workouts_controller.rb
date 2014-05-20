3af1dbdb444d235bb9afbbe5f0350d1b13001c5f
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-20 11:01:01 -0400

    User can edit workout name

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -19,4 +19,22 @@ class WorkoutsController < ApplicationController
     @workout = Workout.find(params[:id])
     @exercises = @workout.exercises
   end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+    if @workout.update(workout_params)
+      redirect_to workout_path(@workout)
+    else
+      render :edit
+    end
+  end
+
+private
+  def workout_params
+    params.require(:workout).permit(:name)
+  end
 end
