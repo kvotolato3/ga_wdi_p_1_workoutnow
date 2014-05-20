9e1220b71366db9714e4e8534fb1c27fd7e9de3d
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-20 14:41:21 -0400

    Modify workout duration...
    ...duration displays in minutes rather than seconds

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,7 +3,7 @@ class Workout < ActiveRecord::Base
   has_many :exercises, through: :workout_exercises
 
   def sum_duration
-    self.duration = self.exercises.sum(:seconds_duration)
+    self.duration = self.exercises.sum(:seconds_duration)/60
     self.save
   end
 end
