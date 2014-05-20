7067cd36e746627bcfd33808281687cfb9d44e2d
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-20 15:14:15 -0400

    User can delete workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -34,6 +34,12 @@ class WorkoutsController < ApplicationController
     end
   end
 
+  def destroy
+    workout = Workout.find(params[:id])
+    workout.destroy
+    redirect_to workouts_path
+  end
+
 private
   def workout_params
     params.require(:workout).permit(:name)
