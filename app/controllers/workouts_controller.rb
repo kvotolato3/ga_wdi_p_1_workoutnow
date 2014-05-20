90744637b3daf8416f5d15d93cd2de6ee382d582
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-19 22:29:25 -0400

    Redirect save to workouts index rather than back

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,13 +1,18 @@
 class WorkoutsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @workouts = Workout.where(user_id: current_user)
+  end
+
   def create
-    authenticate_user!
-    @workout = Workout.new(user_id: current_user)
+    @workout = Workout.new(user_id: current_user.id)
+    binding.pry
     @workout.save
     @exercises = params[:workout]["exercise_ids"].split(',')
     @exercises.each do |exercise|
       @workout.exercises << Exercise.find(exercise.to_i)
     end
     flash[:notice] = 'Workout has been successfully saved!'
-    redirect_to :back
+    redirect_to workouts_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'exercises#index'
+  devise_for :users
+
   resources :exercises
   resources :workouts
-
 end
